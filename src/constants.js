29d51475d7e15cc51ebf87d35cde8e6dea750f37
Reinhold <reinholandia@gmail.com>
2020-06-02 21:11:14 -0500

    adafd

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 import { path } from 'path'
 
 export const PORT = process.env.PORT 
-	? path.join('/', process.env.PORT)
+	? 'https://nameless-badlands-71015.herokuapp.com/'
 	: 'http://localhost:' + process.env.REACT_APP_API_PORT
