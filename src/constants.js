a3e047e71a4fb7aced634d34a64b3771eaf91560
Reinhold <reinholandia@gmail.com>
2020-06-02 21:20:09 -0500

    asd

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 import { path } from 'path'
 
-export const PORT = process.env.PORT === 5000
-	? __dirname
-	: 'http://localhost:' + process.env.REACT_APP_API_PORT
+export const PORT = process.env.REACT_APP_API_PORT === 5000
+	? process.env.REACT_APP_API_PORT
+	: ''
