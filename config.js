e8888d0abf7d5296c80836fd9ec8cb70f5a1ec3a
Reinhold <reinholandia@gmail.com>
2020-06-03 08:45:51 -0500

    add env vars switch for dev/prod

---
+++ config.js
@@ -0,0 +1,29 @@
+'use strict'
+require('dotenv').config({path: '.env.development'})
+module.exports = function(){
+	switch(process.env.REACT_APP_ENVIRONMENT){
+		case 'development':
+			return{
+				ENV: 'development',
+				PORT: process.env.REACT_APP_API_PORT,
+				MONGO_URL: process.env.REACT_APP_MONGO_URL,
+				P_APP_ID: process.env.REACT_APP_PUSHER_APP_ID,
+				P_KEY: process.env.REACT_APP_PUSHER_KEY,
+				P_SECRET: process.env.REACT_APP_PUSHER_SECRET,
+				P_CLUSTER: process.env.REACT_APP_PUSHER_CLUSTER,
+				API_URL: 'http://localhost:' + process.env.REACT_APP_API_PORT
+			}
+		case 'production':
+			return{
+				ENV: 'production',
+				PORT: process.env.REACT_APP_API_PORT,
+				MONGO_URL: process.env.REACT_APP_MONGO_URL,
+				P_APP_ID: process.env.REACT_APP_PUSHER_APP_ID,
+				P_KEY: process.env.REACT_APP_PUSHER_KEY,
+				P_SECRET: process.env.REACT_APP_PUSHER_SECRET,
+				P_CLUSTER: process.env.REACT_APP_PUSHER_CLUSTER,
+				API_URL: ''
+			}
+		default: throw new Error('missing envs')
+	}
+}
